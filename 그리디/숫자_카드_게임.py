54c9f6ae5cb6de44d87867121319390725311b3a
naerim <naerim1119@gmail.com>
2022-03-04 21:28:30 +0900

    그리디 - 숫자 카드 게임 풀이

--- 그리디/숫자_카드_게임.py
+++ 그리디/숫자_카드_게임.py
@@ -21,3 +21,33 @@ for i in range(n):
     else:
         if min[i] > min[i-1]: # 행의 최솟값이 자신보다 크면 바꾸기
             result = min[i]
+
+####################################
+## min() 함수를 이용하는 답안
+n,m = map(int, input().split())
+
+result = 0
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+print(result)
+
+####################################
+## 2중 반복문 구조를 이용하는 답안
+n,m = map(int, input().split())
+
+result = 0
+# 한 줄 씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+print(result)
