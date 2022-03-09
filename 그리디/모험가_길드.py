c997c9d805b813e951544840ff2455db9a49843c
naerim <naerim1119@gmail.com>
2022-03-09 16:43:22 +0900

    모험가 길드 풀이

--- 그리디/모험가_길드.py
+++ 그리디/모험가_길드.py
@@ -12,3 +12,17 @@ while i < n:
     count += 1
     
 print(count)
+
+####################################
+# 다른 답안
+arr.sort()
+
+result = 0 # 총 그룹 수
+count = 0 # 현재 그룹에 포함된 모험가 수
+
+for i in arr: # 공포도 낮은 것부터 확인
+    count += 1 # 현재 그룹에 해당 모함가를 포함시킴
+    if count >= i: # 현재 그룹의 모험가 수가 현재의 공포도 이상이면, 그룹 결성
+        result += 1
+        count = 0
+print(result)
