30f3bbdeb16322ef1ba8848da91da6c784755972
naerim <naerim1119@gmail.com>
2022-03-15 20:28:03 +0900

    문자열 뒤집기 풀이

--- 그리디/문자열_뒤집기.py
+++ 그리디/문자열_뒤집기.py
@@ -27,3 +27,17 @@ for i in range(1, len(S)):
         result +=1
         
 print(result)
+
+####################################
+s = input()
+n = list(map(int,s))
+
+count = [0,0]
+
+count[n[0]] += 1
+for i in range(1, len(s)):
+    if n[i-1] != n[i]:
+        count[n[i]] += 1
+        
+print(min(count))
+# 0을 뒤집는 횟수와 1을 뒤집는 횟수 비교
