3b2715e41ec84d6fdbb348b9df2e5847275c20e4
naerim <naerim1119@gmail.com>
2022-03-17 21:10:25 +0900

    볼링공 고르기 풀이

--- 그리디/볼링공_고르기.py
+++ 그리디/볼링공_고르기.py
@@ -11,3 +11,20 @@ for i in range(0, len(K)):
             continue
 print(result)
     
+####################################
+# 위의 코드는 시간초과 나는듯?
+n,m = map(int, input().split())
+data = list(map(int, input().split()))
+
+# 1부터 10까지의 무게 담기
+array = [0] * 11
+
+# 각 무게에 해당하는 볼링공의 개수 카운트
+for x in data:
+    array[x] += 1
+
+result = 0
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m+1):
+    n -= array[i] # 무게가 1인 볼링공의 개수 재외(A가 선택할 수 있는 갯수)
+    result += array[i] * n # B가 선택하는 경우의 수와 곱하기
