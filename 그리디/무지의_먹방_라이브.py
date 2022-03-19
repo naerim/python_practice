f17038046e57225f1db8a75439b3688a4070011b
naerim <naerim1119@gmail.com>
2022-03-19 17:59:39 +0900

    무지의 먹방 라이브 풀이

--- 그리디/무지의_먹방_라이브.py
+++ 그리디/무지의_먹방_라이브.py
@@ -27,3 +27,40 @@ def solution(food_times, k):
 
 array = [3,1,2]
 print(solution(array, 8))
+
+####################################
+# 위의 코드는 정확도 100, 효율성 0
+
+import heapq
+
+def solution1(food_times, k):
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
+    if sum(food_times) <= k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i+1))
+    
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    length = len(food_times) # 남은 음식의 개수
+    
+    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 햔재 음식 개수와 k 비교
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정
+    
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+    result = sorted(q, key=lambda x: x[1]) # 음식의 번호 기준으로 정렬
+    return result[(k - sum_value) % length][1]
+    
+# heapq
+# heapq.heappop(heap) : heap에서 가장 작은 원소를 pop & return
+
+# sorted
+# sorted(정렬할 데이터, key 파라미터)
