6d6c5b95fbf3d03e12d1dcaaa42170508de7e326
naerim <naerim1119@gmail.com>
2022-04-02 19:46:36 +0900

    게임개발 풀이

--- 구현/게임개발.py
+++ 구현/게임개발.py
@@ -1,41 +1,59 @@
-from calendar import c
+from operator import countOf
 
 
-size = list(map(int, input().split()))
-character = list(map(int, input().split()))
+n,m = map(int, input().split())
 
-place = [] # 맵
-# 맵의 크기만큼 입력받기
-for i in range(size[0]):
-    place.append(list(map(int, input().split())))
-        
-# 바라보고 있는 방향 기준 계산
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터 x좌표, y좌표, 방향
+x,y,direction = map(int, input().split())
+d[x][y] = 1 # 현재 좌표 방문 처리
+
+# 전체 맵 정보
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-current = character[2] # 현재 바라보고 있는 방향
 
-result = 0
-x = character[0]
-y = character[1]
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
 
-# 캐릭터 움직이기
-
-
-# 현재 방향 기준으로 반시계방향 확인하기
-def check_direction(current):
-    count = 1
-    while count < 5:
-        if current > 3:
-            current = 0
-        next = place[dx[current]][dy[current]] # 다음이 바디인지 육지인지
-        if next != 0:
-            # 육지이면 이동
-            x += dx
-            y += dy
-            result += 1
-            break
-        else: 
-            # 바다면 방향 변경
-            current += 1
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
         count += 1
-    return current
+        turn_time = 0
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
+    
+print(countOf)
