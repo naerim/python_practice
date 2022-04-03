4050cf593b80cfb3591616b1593913af0f3d738a
naerim <naerim1119@gmail.com>
2022-04-03 22:45:02 +0900

    럭키 스트레이트 풀이

--- 구현/럭키 스트레이트.py
+++ 구현/럭키 스트레이트.py
@@ -16,3 +16,23 @@ if left_sum == right_sum:
     print("LUCKY");
 else:
     print("READY")
+    
+####################################
+# 책 풀이
+
+n = input()
+length = len(n)
+summary = 0
+
+for i in range(length // 2):
+    summary += int(n[i])
+    
+for i in range(length // 2, length):
+    summary -= int(n[i])
+    
+if summary == 0:
+    print("LUCKY");
+else:
+    print("READY")
+    
+# // : 나누기 연산 후 소수점 이하의 수를 버리고, 정수 부분의 수만 구함
