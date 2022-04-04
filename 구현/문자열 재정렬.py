e01f48c66d3105be0b9941560ad4308d106cfed1
naerim <naerim1119@gmail.com>
2022-04-04 20:54:06 +0900

    문자열 재정렬 풀이

--- 구현/문자열 재정렬.py
+++ 구현/문자열 재정렬.py
@@ -15,4 +15,27 @@ new_arr = sorted(arr)
 
 for i in new_arr:
     print(i, end='')
-print(num)
+print(num) # >> 숫자가 없을때도 고려해줘야 함..!
+
+####################################
+# 책 풀이
+
+data = input()
+result = []
+value = 0
+
+for x in data:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+
+result.sort()
+
+# 숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+if value != 0:
+    result.append(str(value))
+    
+# 최종 결과 출력(리스트를 문자열로 변환하여 출력)
+print(''.join(result))
+# join(리스트)를 이용하면 매개변수로 들어온 ['a', 'b', 'c'] 이런 식의 리스트를 'abc'의 문자열로 합쳐서 반환해주는 함수
